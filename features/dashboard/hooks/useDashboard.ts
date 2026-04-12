dabeb5ff936af677e6d576a07216681e87cc6e1b
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2026-04-12 12:29:59 +0800

    feat: refactor dashboard UI with billing and meter reading data fetching hooks with focuse on single instance

--- features/dashboard/hooks/useDashboard.ts
+++ features/dashboard/hooks/useDashboard.ts
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react"
 import { Billing, MeterReading } from "@/features/billings/type"
 import { getCurrentUser } from "@/supabase/user"
-import { getBillings } from "@/supabase/bills"
-import { getMeterReadings } from "@/supabase/readings"
+import { getLatestBilling } from "@/supabase/bills"
+import { getLatestMeterReading, getMeterReadings } from "@/supabase/readings"
 
 export const useDashboard = () => {
-    const [readings, setReadings] = useState<MeterReading[]>([]);
-    const [billings, setBillings] = useState<Billing[]>([]);
+    const [reading, setReading] = useState<MeterReading>();
+    const [billing, setBilling] = useState<Billing>();
     
     useEffect(() => {
         const fetchReadings = async () => {
-        const currentUser = await getCurrentUser();
-        const readings = await getMeterReadings(currentUser.consumer_id) as MeterReading[];
-        setReadings(readings);
+            const currentUser = await getCurrentUser();
+            const reading = await getLatestMeterReading(currentUser.consumer_id) as MeterReading;
+            setReading(reading);
         }
         const fetchBillings = async () => {
-        const currentUser = await getCurrentUser();
-        const billing = await getBillings(currentUser.consumer_id) as Billing[];
-        setBillings(billing);
+            const currentUser = await getCurrentUser();
+            const billing = await getLatestBilling(currentUser.consumer_id) as Billing;
+            setBilling(billing);
         }
         fetchReadings();
         fetchBillings();
     }, []);
 
     return {
-        readings,
-        billings
+        reading,
+        billing
     }
 }
